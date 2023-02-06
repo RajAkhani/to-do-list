01bfdbde9d6c1c119a958c12271737f864dd6146
RajAkhani <akhaniraj24@gmail.com>
2023-02-06 11:52:07 +0530

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddItemsComponent } from './add-items/add-items.component';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { EditComponent } from './list-items/edit/edit.component';
+import { DeleteComponent } from './list-items/delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddItemsComponent,
+    ListItemsComponent,
+    EditComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
