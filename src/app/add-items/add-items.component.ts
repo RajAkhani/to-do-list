f8b5afccd82ec87390acaddbde7a4a1b5b9124cd
RajAkhani <akhaniraj24@gmail.com>
2023-02-08 09:15:17 +0530

    a

--- src/app/add-items/add-items.component.ts
+++ src/app/add-items/add-items.component.ts
@@ -1,42 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CrudService} from "../crud.service";
-import {Task} from "../model/task";
+
 
 @Component({
   selector: 'app-add-items',
   templateUrl: './add-items.component.html',
   styleUrls: ['./add-items.component.css']
 })
-export class AddItemsComponent {
-  taskObj: Task = new Task();
-  taskArr: Task[] = [];
+export class AddItemsComponent implements OnInit{
 
-  addTaskVal: object = {name: '', description: '', status: '', start_date: '', end_date: ''};
-
+  taskObj: TaskObj
   constructor(private crudService: CrudService) {
+    this.taskObj = new TaskObj();
   }
 
   ngOnInit(): void {
-    this.addTaskVal = {name: '', description: '', status: '', start_date: '', end_date: ''};
-    this.taskObj = new Task();
-    this.taskArr = [];
-    // this.getAllTask();
   }
 
-//   getAllTask(){
-//     this.crudService.getAllTask().subscribe(res => {
-//       this.taskArr = res;
-//     }, err => {
-//       alert("Unable to get list of tasks");
-//     });
-//   }
-//   addTask() {
-//     this.taskObj.task_name = this.addTaskVal;
-//     this.crudService.addTask(this.taskObj).subscribe(res => {
-//       this.ngOnInit();
-//       this.addTaskVal = {name:'', description:'',status:'',start_date:'',end_date:''};
-//     }, err => {
-//       alert(err);
-//     })
-//   }
+  getAllTasks(){
+
+  }
+}
+
+export class TaskObj {
+  Name: string;
+  Description: string;
+  Status: string;
+  Start_Date: string;
+  End_Date: string;
+  constructor() {
+    this.Name = "";
+    this.Description="";
+    this.Status="";
+    this.Start_Date="";
+    this.End_Date='';
+  }
+
 }
