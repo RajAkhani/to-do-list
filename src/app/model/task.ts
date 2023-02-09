055fde06d769a4eeb7097c617a9e6f59041c89c9
RajAkhani <akhaniraj24@gmail.com>
2023-02-09 15:06:50 +0530

    end

--- src/app/model/task.ts
+++
@@ -1,8 +0,0 @@
-export class Task {
-  id : number = 0;
-  task_name : string = "";
-  task_desc : string = "";
-  status : string = "";
-  start_date : Date = new Date();
-  end_date : Date = new Date();
-}
